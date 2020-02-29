81795ab1b2e914bc8489f35897db100a59ca2a22
duzisnetto <60482472+duzisnetto@users.noreply.github.com>
2020-02-28 21:16:08 -0300

    Início do trabalho

---
+++ posttit/postit.js
@@ -0,0 +1,20 @@
+var maior = document.getElementById("postIt") 
+var botao = document.createElement('button')
+var inputDeTexto = document.createElement ("input") 
+
+
+
+botao.innerHTML= "Enviar"
+maior.appendChild(botao)
+maior.appendChild(inputDeTexto)
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
